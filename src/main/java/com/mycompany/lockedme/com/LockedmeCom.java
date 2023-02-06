b497d6067198e9a8d320fc872611d5c8d6e4e906
root <ovacto@gmail.com>
2023-02-06 10:40:32 +0300

    intialise of the project

---
+++ src/main/java/com/mycompany/lockedme/com/LockedmeCom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lockedme.com;
+
+/**
+ *
+ * @author muhammad
+ */
+public class LockedmeCom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
